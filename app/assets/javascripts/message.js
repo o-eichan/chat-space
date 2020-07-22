6535225aed162fe39a96723ccc291ec0b94712e7
o-eichan <a_chan1231@icloud.com>
2020-07-22 16:10:07 +0900

    Update function.....
    create.json.jbuilder
    
    create html in json's data
    
    add html in parents element

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,4 +1,47 @@
 $(function(){
+
+  function buildHTML(message){
+    // 「もしメッセージに画像が含まれていたら」という条件式
+    if (message.image) {
+      let html =  
+        `<div class="Message-box">
+          <div class="Message-info">
+            <div class="MessageInfo__userName">
+              ${message.user_name}
+            </div>
+            <div class="MessageInfo__date">
+              ${message.created_at}
+            </div>
+          </div>
+          <div class="Message">
+            <p class="Message__content">
+              ${message.content}
+            </p>
+            <img class="Message__image" src="${message.image_url}">
+          </div>
+        </div>`
+      return html;
+    } else {
+      let html = 
+        `<div class="Message-box">
+          <div class="Message-info">
+            <div class="MessageInfo__userName">
+              ${message.user_name}
+            </div>
+            <div class="MessageInfo__date">
+              ${message.created_at}
+            </div>
+          </div>
+          <div class="Message">
+            <p class="Message__content">
+              ${message.content}
+            </p>
+          </div>
+        </div>`
+        return html;
+    }
+  }
+  
   $(".Form").on("submit", function(e){
     e.preventDefault()
     let formData = new FormData(this);
@@ -11,5 +54,12 @@ $(function(){
       processData: false,
       contentType: false
     })
+    .done(function(data){
+      let html = buildHTML(data);
+      $(".main-chat__message-list").append(html);
+      $('form')[0].reset();
+    })
+
   })
 });
+

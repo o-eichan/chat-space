eedb2003cab9ac790466367d99d2f17bb2059d8f
o-eichan <a_chan1231@icloud.com>
2020-07-23 19:43:40 +0900

    add click actiion Event & change Form contents ... add current_user
    $("#UserSearchResult").on('click', ".ChatMember__add", function()
    
    (".ChatMember").on('click', ".ChatMember__remove", function()

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -19,6 +19,18 @@ $(function(){
     $("#UserSearchResult").append(html);
   }
 
+  function addMember(name, id) {
+    let html = `
+                <div class="ChatMember">
+                  <p class="ChatMember__name">${name}</p>
+                  <input name="group[user_ids][]" type="hidden" value="${id}" />
+                  <div class="ChatMember__remove ChatMember__button">削除</div>
+                </div>
+                `;
+    $(".ChatMembers").append(html);
+    
+  }
+
   $("#UserSearch__field").on("keyup", function(){
     let input = $("#UserSearch__field").val();    //フォームの値を取得して変数に代入する
     $.ajax({
@@ -43,4 +55,15 @@ $(function(){
       alert("通信エラーです。ユーザーが表示できません。");
     });
   })
+
+  $("#UserSearchResult").on('click', ".ChatMember__add", function(){
+    const userName = $(this).attr("data-user-name");
+    const userId = $(this).attr("data-user-id");
+    $(this).parent().remove();
+    addMember(userName, userId);
+  });
+
+  $(".ChatMember").on('click', ".ChatMember__remove", function(){
+    $(this).parent().remove();
+  });
 });

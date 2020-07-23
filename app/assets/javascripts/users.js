5f13702cd51156697a786b283b88449f801150b0
o-eichan <a_chan1231@icloud.com>
2020-07-23 14:06:52 +0900

    Complete Increment Reaserch

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -1,6 +1,46 @@
 $(function(){
 
+  function addUser(user) {
+    let html = `
+                <div class="ChatMember clearfix">
+                  <p class="ChatMember__name">${user.name}</p>
+                  <div class="ChatMember__add ChatMember__button" data-user-id="${user.id}" data-user-name="${user.name}">追加</div>
+                </div>
+                `;
+    $("#UserSearchResult").append(html);
+  }
+
+  function addNoUser() {
+    let html = `
+                <div class="ChatMember clearfix">
+                  <p class="ChatMember__name">ユーザーが見つかりません</p>
+                </div>
+                `;
+    $("#UserSearchResult").append(html);
+  }
+
   $("#UserSearch__field").on("keyup", function(){
     let input = $("#UserSearch__field").val();    //フォームの値を取得して変数に代入する
+    $.ajax({
+      type: "GET",    //HTTPメソッド
+      url: "/users",       //users_controllerの、indexアクションにリクエストの送信先を設定する
+      dataType: 'json',
+      data: { keyword: input },   //テキストフィールドに入力された文字を設定する
+    })
+    .done(function(users) {
+      $("#UserSearchResult").empty();
+      if (users.length !== 0) {
+        users.forEach(function(user) {
+          addUser(user);
+        });
+      } else if (input.length == 0) {
+        return false;
+      } else {
+        addNoUser();
+      }
+    })
+    .fail(function() {
+      alert("通信エラーです。ユーザーが表示できません。");
+    });
   })
 });

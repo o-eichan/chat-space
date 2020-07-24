b0012a7ab95e0f3ea9393929760e24eb71257d27
o-eichan <a_chan1231@icloud.com>
2020-07-24 15:45:35 +0900

    add...Show reloadMessage

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,9 +1,10 @@
 $(function(){
+  let last_message_id = $('.MessageBox:last').data("message-id") || 0;
 
   function buildHTML(message){
     if (message.image) {
       let html =  
-        `<div class="Message-box">
+        `<div class="Message-box" data-message-id=${message.id}>
           <div class="Message-info">
             <div class="MessageInfo__userName">
               ${message.user_name}
@@ -22,7 +23,7 @@ $(function(){
       return html;
     } else {
       let html = 
-        `<div class="Message-box">
+        `<div class="Message-box" data-message-id=${message.id}>
           <div class="Message-info">
             <div class="MessageInfo__userName">
               ${message.user_name}
@@ -66,5 +67,36 @@ $(function(){
       $(".Send-btn").prop("disabled", false);
     })
   })
+
+  let reloadMessages = function() {
+    //カスタムデータ属性を利用し、ブラウザに表示されている最新メッセージのidを取得
+    let last_message_id = $('.MessageBox:last').data("message-id") || 0;
+    $.ajax({
+      //ルーティングで設定した通り/groups/id番号/api/messagesとなるよう文字列を書く
+      url: "api/messages",
+      //ルーティングで設定した通りhttpメソッドをgetに指定
+      type: 'get',
+      dataType: 'json',
+      //dataオプションでリクエストに値を含める
+      data: {id: last_message_id}
+    })
+    .done(function(messages) {
+      // 更新するメッセージがなかった場合は.doneの後の処理が動かないようにする
+      if (messages.length !== 0) {
+        //追加するHTMLの入れ物を作る
+        let insertHTML = '';
+        //配列messagesの中身一つ一つを取り出し、HTMLに変換したものを入れ物に足し合わせる
+        $.each(messages, function(i, message) {
+          insertHTML += buildHTML(message)
+        });
+        //メッセージが入ったHTMLに、入れ物ごと追加
+        $('.main-chat__message-list').append(insertHTML);
+      }
+    })
+    .fail(function() {
+      alert('error');
+    });
+  };
+  setInterval(reloadMessages, 7000);
 });
 
